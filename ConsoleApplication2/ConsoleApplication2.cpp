309e975d95077aaf17c7f589c9527b02799dd18b
Daniil <Daniil@DESKTOP-A9S284V>
2022-07-14 19:50:13 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int big = 2000000000;
+    int small = 1000000000;
+    string text = "Text";
+    
+    double sum = 0;
+    long razn = 0;
+
+    sum = (double)big + small;
+    razn = (small - big);
+
+    cout << "sum=" << sum << "\t" << "razn=" << razn << "\n";
+    cout << text << endl;
+
+    return 0;
+}
